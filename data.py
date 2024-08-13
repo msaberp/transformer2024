7714f5459085674cd9333200eec83488bf3e8d0e
msaberp <m.saberpourheydari@gmail.com>
2024-08-13 15:48:02 +0200

    feat: using `hydra` as a config management system instead of directly setting configuration in the `conf.py` file.

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
-from conf import *
 from util.data_loader import TranslationDataLoader
 from util.tokenizer import Tokenizer
+from hydra import compose, initialize
+
+with initialize(config_path="config", version_base="1.3"):
+    cfg = compose(config_name="config")
 
 tokenizer = Tokenizer()
 loader = TranslationDataLoader(language_pair=('en', 'de'),
@@ -9,9 +12,9 @@ loader = TranslationDataLoader(language_pair=('en', 'de'),
 
 train_data, val_data, test_data = loader.make_dataset()
 loader.build_vocab(train_data=train_data)
-train_iter = loader.make_test_iter(train_data, batch_size=batch_size)
-valid_iter = loader.make_test_iter(val_data, batch_size=batch_size)
-test_iter = loader.make_test_iter(test_data, batch_size=batch_size)
+train_iter = loader.make_test_iter(train_data, batch_size=cfg.dataset.batch_size)
+valid_iter = loader.make_test_iter(val_data, batch_size=cfg.dataset.batch_size)
+test_iter = loader.make_test_iter(test_data, batch_size=cfg.dataset.batch_size)
 
 src_pad_idx = loader.source_vocab['<pad>']
 trg_pad_idx = loader.target_vocab['<pad>']
--- util/logger.py
+++ util/logger.py
@@ -2,6 +2,6 @@ import logging
 
 
 def get_logger(name: str) -> logging.Logger:
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(level=logging.INFO, format='[%(asctime)s][%(name)s][%(levelname)s] - %(message)s')
     logger = logging.getLogger(name)
     return logger
